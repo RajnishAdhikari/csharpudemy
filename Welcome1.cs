acd90bcfb227eee013c544633b7d75c75650506b
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-04-28 09:35:40 +0545

    simple implementation

--- Welcome1.cs
+++ Welcome1.cs
@@ -6,7 +6,9 @@ namespace Welcome1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hey");
+            Console.WriteLine("hey"); // after the statement executes it curser will be in the same line 
+            // Console.Write("welcome to csharp ");                      curser will be in next line 
+            Console.WriteLine("welcome\n to \n csharp \n course\n hahaha "); // \n helps to go to new line
             Console.ReadLine();
         }
     }
