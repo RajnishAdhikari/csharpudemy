54ff39bbbcc1e2146bcc17570aa3c36c6cb933fd
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-04-28 09:17:00 +0545

    first commit

---
+++ Welcome1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Welcome1
+{
+    internal class Welcome1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hey");
+            Console.ReadLine();
+        }
+    }
+}
