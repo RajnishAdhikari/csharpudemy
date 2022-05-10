0e7d23a00cda31fbacda4053a55415c300c7cf09
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-05-10 12:58:11 +0545

    multiple interface

--- Interface_Multiple_Inheritance.cs
+++ Interface_Multiple_Inheritance.cs
@@ -6,17 +6,42 @@ using System.Threading.Tasks;
 
 namespace Welcome1
 {
+    // this is the example of multiple interface 
     public interface compute1
     {
-        public int a;
-        public int b;
-        public void addition(int a, int b)
+        void findsum(int x, int y);
+        void finddifference(int x, int y);
+    }
+    public interface compute2
+    {
+        void finddivision(int x, int y);
+        void findmodulo(int x, int y);
+    }
+
+    class practical : compute1, compute2
+    {
+        public void finddifference(int x, int y)
         {
-            Console.WriteLine($"sum of {a} and {b} is {a + b}");
+            Console.WriteLine($"difference: {x - y}");
+        }
+
+        public void finddivision(int x, int y)
+        {
+            Console.WriteLine($"division: {x / y}");
+        }
+
+        public void findmodulo(int x, int y)
+        {
+            Console.WriteLine($"modulo: {x % y}");
+        }
+
+        public void findsum(int x, int y)
+        {
+            Console.WriteLine($"Sum: {x + y}");
         }
     }
+    internal class multiple_inheritance
+    {
+    }
 }
-    //internal class Interface_Multiple_Inheritance
-    //{
-    //}
 
